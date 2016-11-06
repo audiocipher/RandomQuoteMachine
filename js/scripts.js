22156ba4cd369f4384cdd436b59257dee4a0b633
AJ McClure <audiocipher@gmail.com>
2016-11-05 22:18:37 -0500

    added long hyphen to twitter functionality

--- js/scripts.js
+++ js/scripts.js
@@ -94,7 +94,7 @@ $(function(){
   var update_href = function(){
     let $elTweet = $('#tweet'); // reference to the DOM element
     let temp_obj = quote_machine.fetch_quote(); // temporary quote object to get quote and source from
-    let text = temp_obj.getQuote() + ' ' + temp_obj.getSource(); // concat quote and source
+    let text = temp_obj.getQuote() + ' \u2015' + temp_obj.getSource(); // concat quote and source
     let sub_url = encodeURIComponent(text); // convert to "href friendly" text
     let the_url = 'https://twitter.com/intent/tweet?text=' +
                   sub_url +
